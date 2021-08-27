16af0be8b273a72abbbb280204e6bddee14de4cb
Luke De Vuono <flamingmagma@gmail.com>
2021-08-28 04:28:12 +1000

    First Commit

---
+++ pages/_boards/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    BOARD
+    
+    <div v-for="(tutorial, index) of tutorials" :key="index">
+      {{tutorial}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: 'ad',
+      route: this.$route,
+      tutorials: null
+    }
+  },
+  async mounted() {
+    const ip = await this.$axios.$get('http://localhost:8080/api/tutorials')
+    this.tutorials = ip;
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Create Board
+    </v-card-title>
+
+    <v-card-text>
+      asdsa
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: 5,
+      ip: null
+    }
+  },
+  async mounted() {
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      this.ip = ip
+  }
+}
+</script>
+
+<style>
+
+</style>
